88ea9c7cbc620ac993c9b46d04f4185be9430b1b
arceso <arcesone@gmail.com>
2016-11-24 10:25:25 +0100

    Fixed my lil' bug

--- main.py
+++ main.py
@@ -4,18 +4,10 @@ from modulos.barberia import Barberia
 from modulos.cliente import Cliente
 from modulos.barbero import Barbero
 from time import sleep
-from colorama import init, Fore, Back, Style
 
 #El main simplemente tiene lo básico para iniciar las demás clases
 
 def main():
-    '''
-    Este init es del paquete colorama (sirve para tener colores en las terminales)
-    Sirve para inicializar sus variables internas (para que sepa en que entorno
-    se encuentra).
-    '''
-    init()
-
     '''
     Asi es como se declaran variables globales para que todas las funciones
     puedan utilizarlas.
@@ -23,10 +15,8 @@ def main():
     global barberia, barbero, numeroSillas
 
     #Numero de sillas disponibles para esperar, si sobrepasa no se aceptan más clientes
-    #numeroSillas = random.randrange(1, 5)
-    numeroSillas = 1
-    print(Back.BLUE + "El máximo de clientes es: ", numeroSillas, ".")
-    print(Style.RESET_ALL)
+    numeroSillas = random.randrange(1, 5)
+    print("El máximo de clientes es: ", numeroSillas, ".")
 
     #Instanciamos las clases barberia y barbero
     #Barberia recibe el numero de asientos disponibles
@@ -51,21 +41,18 @@ def iniciar():
     global barberia, barbero, numeroSillas
     while(True):
         #Mandamos a dormir el bucle para que la creación de clientes tenga algo de tiempo
-        sleep(random.uniform(1, 1.5))
+        sleep(random.uniform(.25, .5))
         cliente = Cliente(barberia, barbero)
         cliente.setName("Cliente " + cliente.getName())
         #Si hay sitio en la barberia se crea un nuevo proceso que esperará a poder cortarse el pelo
         #sino se perderá ese cliente
         if barberia.getCuantosClientes() < numeroSillas:
             #El cliente estará constantemente viendo si puede o no entrar a la silla principal
-            print(Back.YELLOW + "Ha llegado el ", cliente.getName(), ".")
-            print(Style.RESET_ALL)
+            print("Ha llegado el ", cliente.getName(), ".")
             cliente.start()
 
         else:
-            print(Back.RED + cliente.getName() + "se fue por no tener sitio.")
-            print(Style.RESET_ALL)
-
+            print(cliente.getName() + " se fue por no tener sitio.")
 
 #Es una forma de decirle a python que la función main() será la principal
 if __name__ == "__main__":
